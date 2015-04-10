d6e287942377deb0d0c7f9243ff5b0528b14743a
Rafael Guedes <faeljfa@hotmail.com>
2015-04-10 15:48:40 -0300

    commitando para criar a tag versão 0.0.1

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-echo phpinfo();
+#Eu ainda não havia visto a parte do curso referente à criação de tags.
+#O formulário para o envio está disponível no item projeto fase 1 na seção
+#anterior.
+
+
+//Criando um código qualquer no arquivo.
+$array = array("0"=>"a", "1"=>"b", "2"=>"c", "3"=>"d", "4"=>"e", "5"=>"f");
+
+foreach ($array as $chv=>$dado) {
+    echo $chv . " - " . $dado."<br />";
+}
