dab4196d40a02947b9ae8011173b50d6164d1d95
Rafael Guedes <faeljfa@hotmail.com>
2015-04-10 16:09:00 -0300

    Mudando a versão 'major' do sistema. Um novo release será lançado em breve.

--- index.php
+++ index.php
@@ -3,18 +3,4 @@
 #O formulário para o envio está disponível no item projeto fase 1 na seção
 #anterior.
 
-header("Content-Type: text/html; charset=UTF-8",true);
-
-//Criando um código qualquer no arquivo.
-$array = array("a"=>"a", "b"=>"b", "c"=>"c", "d"=>"d");
-
-foreach ($array as $chv=>$dado) {
-    echo $chv . " - " . $dado."<br />";
-}
-
-//Criando mais qualquer coisa no arquivo.
-$array = array("1"=>"Bisão", "2"=>"Elefante", "3"=>"pato", "4"=>"Urso");
-
-foreach ($array as $chv=>$dado) {
-    echo $chv . " - " . $dado."<br />";
-}
+echo phpinfo();
