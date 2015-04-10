bb4abd1b05e60899649493e2aaec5dfa467ccd3c
Rafael Guedes <faeljfa@hotmail.com>
2015-04-10 14:39:30 -0300

    Substituindo o phpinfo() por um código qualquer.

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
+$array = array("0"=>"a", "1"=>"b", "2"=>"c", "3"=>"d", "4"=>"e", "5"=>"f");
 
-echo "Hello World!!!";
+foreach ($array as $chv=>$dado) {
+    echo $chv . " - " . $dado."<br />";
+}
